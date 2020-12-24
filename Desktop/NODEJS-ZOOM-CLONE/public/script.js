a0a12bbf9c60347b48a867ba61a0b88df7a92a3b
jaypatel1447j.p@gmail.com <jaypatel1447j.p@gmail.com>
2020-12-24 01:15:33 -0500

    Multi user

--- Desktop/NODEJS-ZOOM-CLONE/public/script.js
+++ Desktop/NODEJS-ZOOM-CLONE/public/script.js
@@ -1,12 +1,24 @@
 
+
+const socket  = io('/');
 //  getElementById(video-grid) where video-grid is id property of room.ejs 
 // continue: so you can pass id here to call and set video in grid formate this is the
+
+
+
 // great functionality of ejs - Embedded JavaScript
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 
 // We set our own video to be muted why cz we dont want hear our own voice.
 myVideo.muted = true
+
+var peer = new Peer(undefined, {
+    path: '/pathjs',
+    host: '/', // / means any host it can be heroku, local or vercel or anythink
+    port: '3030'
+}); 
+
 
 let myVideoStream
 // Allow us to give access to steam our own video
@@ -19,11 +31,24 @@ navigator.mediaDevices.getUserMedia({
     addVideoStream(myVideo, stream);
 })
 
+peer.on('open', id => {
+    console.log(id);
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+
+socket.on('user-connected', (userId) => {
+    connectToNewUser(userId);
+})
+
+const connectToNewUser = (userId) => {
+    console.log(userId);
+}
+
 const addVideoStream = (video, stream) => {
         video.srcObject = stream;
         video.addEventListener('loadedmetadata', () => {
             video.play();
         })
         videoGrid.append(video);
-       
 }
--- Desktop/NODEJS-ZOOM-CLONE/server.js
+++ Desktop/NODEJS-ZOOM-CLONE/server.js
@@ -2,6 +2,11 @@ const express  = require('express');
 const app = express();
 const server = require('http').Server(app);
 const {v4: uuidv4} = require('uuid');
+const io = require('socket.io')(server);
+const {ExpressPeerServer} = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 
 
 
@@ -9,6 +14,8 @@ app.set('view engine', 'ejs');
 
 // Notify server that public file are going to be here
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
+
 
 app.get('/', (req,res) => {
     //This will generate Unique Id of Zoom Meeting and render it to /:room
@@ -20,6 +27,13 @@ app.get('/:room', (req, res) => {
     res.render('room', {roomId: req.params.room})
 })
 
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    })
+})
 
 
 server.listen(3030);
